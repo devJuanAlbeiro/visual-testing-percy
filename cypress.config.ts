c8bd5fbdc37b95a28d2b6e9b451e543e2e58b240
Juan Santana Q <juanalbeiro@hotmail.com>
2022-07-11 11:20:31 -0500

    adding yml file for cypress integration change 9

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'https://localhost:3002',
+    baseUrl: 'https://localhost:3000',
     specPattern: 'cypress/integration/**/*.spec.ts',
     supportFile: 'cypress/support/index.ts',
   },
